e950abbabb1a71ba92f1a87fec9e139b99241038
JULIUS KIRIMA <juliuskirima1@gmail.com>
2026-08-05 09:34:21 +0300

    config(extension): add Firebase credentials and API endpoints for extension

---
+++ extension/config.js
@@ -0,0 +1,87 @@
+"use strict";
+
+/* Application */
+
+export const APP_NAME = "FocusShield";
+
+export const APP_VERSION = "1.0.0";
+
+export const EXTENSION_VERSION = "1.0.0";
+
+/* Supported social media */
+
+export const SUPPORTED_SOCIAL_MEDIA = [
+
+    "facebook.com",
+
+    "www.facebook.com",
+
+    "instagram.com",
+
+    "www.instagram.com",
+
+    "tiktok.com",
+
+    "www.tiktok.com",
+
+    "x.com",
+
+    "www.x.com",
+
+    "twitter.com",
+
+    "www.twitter.com",
+
+    "youtube.com",
+
+    "www.youtube.com",
+
+    "snapchat.com",
+
+    "www.snapchat.com",
+
+    "reddit.com",
+
+    "www.reddit.com",
+
+    "linkedin.com",
+
+    "www.linkedin.com",
+
+    "threads.net",
+
+    "www.threads.net"
+
+];
+
+/* Firebase paths */
+
+export const FIREBASE_PATHS = {
+
+    USERS: "users",
+
+    SETTINGS: "settings",
+
+    BLOCKED_SITES: "blockedSites",
+
+    BLOCKED_ATTEMPTS: "blockedAttempts"
+
+};
+
+/* Extension */
+
+export const CHECK_INTERVAL = 1000;
+
+export const TAB_UPDATE_DELAY = 300;
+
+export const BLOCKED_PAGE = "blocked.html";
+
+export const POPUP_PAGE = "popup.html";
+/* Website */
+
+export const WEBSITE_URL = "https://your-domain.com/";
+
+export const DASHBOARD_URL = "https://kirima-julius.github.io/focusshield/dashboard.html";
+/* Alarm */
+
+export const ALARM_NAME = "focusshield-sync";
